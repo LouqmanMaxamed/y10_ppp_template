8ab3229e4be61644b769ed3260deb99d43120865
Louqman <lmaxamed27@kellettschool.com>
2025-06-25 14:04:20 +0800

    made a plan for minesweeper

---
+++ Minesweeper/minesweeper.py
@@ -0,0 +1,38 @@
+
+# Make a settings board for each difficulty
+
+
+# -------- Easy --------
+
+easy = {"gridX":9, 
+        "gridY":9, 
+        "numOfMines":3,
+        "maxConsective":2,
+        }
+
+
+# -------- Medium --------
+
+
+
+# -------- Hard --------
+
+
+
+# -------- EXTREME --------
+
+# Function for grid-making 
+    # Make a editable grid size (based on difficulty level: Easy(9x9) Medium (16x16) Hard(32x32))
+
+# Function for bomb placing
+    # Randomly place bombs but if there is more than x number of bombs consectively (x is based on the difficulty) stop adding bombs.
+
+# Function for checking bombs nearby already detected spaces
+    # If the column and row of a space is called then check if that space has a bomb if not display the number of bombs nearby it
+
+# Function for choosing spaces
+    # Ask the user for column then row
+
+# Function for flag placing
+    # 
+#
